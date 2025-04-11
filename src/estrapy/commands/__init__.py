a35e1b5540720f54489d3edd1dd34320f3959f85
Marco Stecca <marco.stecca99@gmail.com>
2025-04-11 12:36:30 +0200

    Added fourier and phase commands to command registry

--- src/estrapy/commands/__init__.py
+++ src/estrapy/commands/__init__.py
@@ -1,7 +1,10 @@
-from .filein import FileIn, BatchIn
 from .align import Align, Edge
+from .filein import FileIn, BatchIn
+from .fourier import Fourier
 from .preedge import PreEdge
 from .postedge import PostEdge
+from .phase import Phase
+
 from ._handler import CommandHandler
 
 commands: dict[str, CommandHandler] = {
@@ -11,4 +14,6 @@ commands: dict[str, CommandHandler] = {
     "edge": Edge(),
     "preedge": PreEdge(),
     "postedge": PostEdge(),
+    "fourier": Fourier(),
+    "phase": Phase(),
 }
