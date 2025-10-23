e2e952a5ba8c2ac6e6f11285ed030429daede82a
Marco Stecca <marco.stecca99@gmail.com>
2025-10-23 10:48:08 +0200

    Stub for  script execution to use asynchronous handling for command execution

--- src/estrapy/dispatcher.py
+++ src/estrapy/dispatcher.py
@@ -1,12 +1,23 @@
-from .grammar.transformer import Script
+import asyncio
+import inspect
+
+from typing import Sequence
+
+from .grammar.transformer import Script, Command, CommandArguments
 from .core.context import Context
 
 from .commands import execute_command, execute_directive
 
+async def execute_script_async(commands: Sequence[Command[CommandArguments]], context: Context) -> None:
+    for command in commands:
+        maybe_coro = execute_command(command, context)
+        if inspect.iscoroutine(maybe_coro):
+            await maybe_coro # type: ignore # TODO remove when these will be coroutines
+
 
 def execute_script(script: Script, context: Context) -> None:
+    # Directives are fast, so we execute them sequentially. They are not performed per-file.
     for directive in script.directives:
         execute_directive(directive, context)
 
-    for command in script.commands:
-        execute_command(command, context)
+    asyncio.run(execute_script_async(script.commands, context))
