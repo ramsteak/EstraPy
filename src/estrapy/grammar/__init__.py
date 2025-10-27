626c76713abfe030200063c20a35c0c807f308e0
Marco Stecca <marco.stecca99@gmail.com>
2025-10-27 14:21:00 +0100

    Remove EstraTransformer from __all__ in grammar module to clean up exports

--- src/estrapy/grammar/__init__.py
+++ src/estrapy/grammar/__init__.py
@@ -2,11 +2,9 @@ from importlib.resources import files
 from lark import Lark
 
 from .indenter import EstraIndenter
-from .transformer import EstraTransformer
 
 __all__ = [
     'file_parser',
-    'EstraTransformer',
 ]
 
 grammar_data = files('estrapy.grammar').joinpath('estrapy.lark').read_text()
