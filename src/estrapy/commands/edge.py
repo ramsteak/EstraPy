2e94667d580ff6e5a4724148a4f92df5460fe41f
Marco Stecca <marco.stecca99@gmail.com>
2025-10-29 18:06:30 +0100

    Implement edge command parser and argument handling with CommandArguments_Edge class

---
+++ src/estrapy/commands/edge.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from lark import Token, Tree
+from typing import Self
+
+from ..core.grammarclasses import CommandArguments, Command
+from ..core.context import Context, ParseContext
+from ..core.number import Number, parse_number
+from ..grammar.commandparser import CommandArgumentParser
+from ..core.misc import parse_edge
+
+
+@dataclass(slots=True)
+class CommandArguments_Edge(CommandArguments):
+    mode: str
+    energy: Number | str
+    search: Number
+    delta: Number
+
+parse_edge_command = CommandArgumentParser(CommandArguments_Edge)
+parse_edge_command.add_argument('mode', type=str, required=False, default=None)
+parse_edge_command.add_argument('energy', '--energy', '--E0', '-E', type=parse_edge, required=False, default=None)
+parse_edge_command.add_argument('search', '--search', type=parse_edge, required=False, default=None)
+parse_edge_command.add_argument('delta', '--delta', '-d', type=parse_number, required=False, default=None)
+
+@dataclass(slots=True)
+class Command_Edge(Command[CommandArguments_Edge]):
+    @classmethod
+    def parse(cls: type[Self], commandtoken: Token, tokens: list[Token | Tree[Token]], parsecontext: ParseContext) -> Self:
+        arguments = parse_edge_command(commandtoken, tokens, parsecontext)
+        return cls(
+            line=commandtoken.line or -1,
+            name=commandtoken.value,
+            args=arguments,
+        )
+
+    def initialize(self, context: Context):
+        pass
+
+    def execute_on(self, page: str, context: Context) -> None:
+        ...  # Implement the execution logic for the 'edge' command here
+    
+    def finalize(self, context: Context) -> None:
+        pass
