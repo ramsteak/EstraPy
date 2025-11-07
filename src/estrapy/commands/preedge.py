603a37e8e7126c4ab9ff4f3a909d5f2f9402cf0e
Marco Stecca <marco.stecca99@gmail.com>
2025-11-07 17:17:03 +0100

    Implement pre-edge correction: add polynomial fitting and column calculations for data processing in execute method.

--- src/estrapy/commands/preedge.py
+++ src/estrapy/commands/preedge.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from lark import Token, Tree
 
 from dataclasses import dataclass
@@ -6,17 +8,15 @@ from typing import Self
 from ..core.grammarclasses import CommandArguments, Command
 from ..core.context import Context, ParseContext
 from ..grammar.commandparser import CommandArgumentParser
-from ..core.number import Number, parse_range
+from ..core.number import Number, parse_range, Unit
+from ..core.datastore import Domain, ColumnDescription, ColumnKind
+from ..core.misc import fmt
 
 
 @dataclass(slots=True)
 class CommandArguments_Preedge(CommandArguments):
     range: tuple[Number, Number]
     degree: int
-    x: str
-    y: str
-    n: str
-
 
 parse_preedge_command = CommandArgumentParser(CommandArguments_Preedge)
 parse_preedge_command.add_argument('range', types=parse_range, nargs=2, required=True)
@@ -25,9 +25,6 @@ parse_preedge_command.add_argument(None, '--constant', '-C', action='store_const
 parse_preedge_command.add_argument(None, '--linear', '-l', action='store_const', dest='degree', const=1, nargs=0)
 parse_preedge_command.add_argument(None, '--quadratic', '-q', action='store_const', dest='degree', const=2, nargs=0)
 parse_preedge_command.add_argument(None, '--cubic', '-c', action='store_const', dest='degree', const=3, nargs=0)
-parse_preedge_command.add_argument('x', '--xaxiscol', type=str, default='E')
-parse_preedge_command.add_argument('y', '--yaxiscol', type=str, default='a')
-parse_preedge_command.add_argument('n', '--newyaxiscol', type=str, default='a')
 
 
 @dataclass(slots=True)
@@ -43,5 +40,51 @@ class Command_Preedge(Command[CommandArguments_Preedge]):
             args=arguments,
         )
 
-    def execute_on(self, page: str, context: Context) -> None:
-        pass
+    def execute(self, context: Context) -> None:
+        log = context.logger.getChild('command.preedge')
+
+        for name, page in context.datastore.pages.items():
+            domain = page.domains[Domain.RECIPROCAL]
+            # TODO: Check that the columns exist and (if needed) E0 is set
+            df = domain.get_columns_data(['E', 'e', 'a'])
+            match self.args.range[0]:
+                case Number(sign=None, value=val, unit=Unit.EV):
+                    idx_l = df['E'] >= val
+                case Number(sign=_, value=val, unit=Unit.EV):
+                    idx_l = df['e'] >= val
+                case Number(sign=_, value=val, unit=Unit.K):
+                    idx_l = df['k'] >= val
+                case _:
+                    raise ValueError(f'Invalid range start "{self.args.range[0]}" for postedge correction.')
+            match self.args.range[1]:
+                case Number(sign=None, value=val, unit=Unit.EV):
+                    idx_u = df['E'] <= val
+                case Number(sign=_, value=val, unit=Unit.EV):
+                    idx_u = df['e'] <= val
+                case Number(sign=_, value=val, unit=Unit.K):
+                    idx_u = df['k'] <= val
+                case _:
+                    raise ValueError(f'Invalid range end "{self.args.range[1]}" for postedge correction.')
+            
+            _region = df[idx_l & idx_u]
+            poly = np.polyfit(_region['e'], _region['a'], deg=self.args.degree)
+            domain.add_column('pre',
+                ColumnDescription(
+                    name='pre',
+                    unit=None,
+                    type=ColumnKind.DATA,
+                    deps=['e'],
+                    calc=lambda d, p=poly: np.polyval(p, d['e']), # type: ignore
+                )
+            )
+            domain.add_column('a',
+                ColumnDescription(
+                    name='a',
+                    unit=None,
+                    type=ColumnKind.DATA,
+                    deps=['a', 'pre'],
+                    calc=lambda d: d['a'] - d['pre'],
+                )
+            )
+            log.debug(f'Applied pre-edge polynomial subtraction of polynomial {fmt.sup.poly([*poly], floatfmt='0.2g')} to page {name}.')
+
