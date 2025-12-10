324f94614aa598b6e7f8089a3c508bd8fa4fc9a0
Marco Stecca <marco.stecca99@gmail.com>
2025-12-10 19:49:47 +0100

    Refactor directive execution to use sorted directives for improved performance

--- src/estrapy/dispatcher.py
+++ src/estrapy/dispatcher.py
@@ -1,18 +1,12 @@
-from typing import Literal, TypeAlias
-
-from .commands import execute_directive
+from .commands import execute_directive, sorted_directives
 from .core.grammarclasses import Script
 from .core.context import Context
 
-SectionKind: TypeAlias = Literal['none', 'sequential', 'threads', 'processes']
-
-
 def execute_script(script: Script, context: Context) -> None:
-    # Directives are fast, so we execute them sequentially. They are not performed per-file.
     with context.timers.time('execution/directives'):
-        for directive in script.directives:
+        for directive in sorted_directives(script.directives):
             execute_directive(directive, context)
-
+ 
     for command in script.commands:
         with context.timers.time(f'execution/{command.name} (line {command.line})'):
             command.execute(context)
