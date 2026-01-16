b579b10ef8dcfce530f7686c5ad7d2b77127e2fc
Marco Stecca <marco.stecca99@gmail.com>
2026-01-16 15:22:32 +0100

    Fix axis indexing in realize_figure function to correctly map axes positions

--- src/estrapy/commands/plot/show.py
+++ src/estrapy/commands/plot/show.py
@@ -6,7 +6,7 @@ def realize_figure(figspec: FigureSpecification):
     """Realize a figure from its specification"""
     fig, _, axes = make_figure_gridspec([a.pos for a in figspec.axes.values()])
     for (col, row), axis_spec in figspec.axes.items():
-        ax = axes[(row, col)]
+        ax = axes[(col, row)]
         for cb in axis_spec.callbacks:
             cb(ax, fig)
     return fig
