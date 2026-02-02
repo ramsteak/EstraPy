449f3fd90c43decfac2b469b7d94ff5981e4f7e3
Marco Stecca <marco.stecca99@gmail.com>
2026-02-02 19:35:02 +0100

    Update copyright year and add banner with version information

--- src/estrapy/__init__.py
+++ src/estrapy/__init__.py
@@ -2,4 +2,15 @@ __version__ = '2.0.0'
 __version_tuple__ = (2, 0, 0)
 
 author = 'Marco Stecca'
-copyright = f'(c) 2024-2025 {author}'
+copyright = f'(c) 2024-2026 {author}'
+
+banner = fr"""
+    ______     __             ____       
+   / ____/____/ /__________ _/ __ \__  __
+  / __/ / ___/ __/ ___/ __ `/ /_/ / / / /
+ / /___(__  ) /_/ /  / /_/ / ____/ /_/ /
+/_____/____/\__/_/   \__,_/_/    \__, /
+                                /____/
+EstraPy - Data Analysis Framework
+Version: {__version__}
+Copyright: {copyright}""".removeprefix("\n")
