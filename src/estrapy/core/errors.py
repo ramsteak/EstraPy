6667c7e785f5449b95ff28d34b82ac17d5029d5f
Marco Stecca <marco.stecca99@gmail.com>
2026-02-02 17:18:35 +0100

    Refactor CommandError class to enhance token handling and update CommandSyntaxError to inherit from CommandParseError

--- src/estrapy/core/errors.py
+++ src/estrapy/core/errors.py
@@ -1,20 +1,30 @@
-from lark import Token
+from lark import Token, Tree
 
+class CommandError(Exception):
+    """Base exception for command-related errors."""
 
-class ParseError(Exception):
-    """Exception raised for errors during parsing."""
-
-    def __init__(self, message: str, token: Token | None = None):
+    def __init__(self, message: str, token: Token | Tree[Token] | list[Token | Tree[Token]] | None = None):
         super().__init__(message)
         self.token = token
-
+    
     def __str__(self) -> str:
-        if self.token is not None:
-            return f'{self.args[0]} (at line {self.token.line}, column {self.token.column})'
-        return self.args[0]
+        match self.token:
+            case None:
+                # No token information available
+                return self.args[0]
+            case Token():
+                # Single token information available
+                return f'{self.args[0]} (at line {self.token.line}, column {self.token.column})'
+            case Tree() | list():
+                # Tree node information available. Gather all tokens and find the maximum span.
+                return f'{self.args[0]}'
 
 
-class CommandSyntaxError(ParseError):
+class CommandParseError(CommandError):
+    """Exception raised for errors during parsing."""
+
+
+class CommandSyntaxError(CommandParseError):
     """Exception raised for syntax errors in commands."""
 
 
