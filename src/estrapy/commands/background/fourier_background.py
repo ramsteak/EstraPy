Add inverse Fourier transform function and update Fourier computation to correct imaginary sign

src/estrapy/commands/background/fourier_background.py
@@ -11,7 +11,7 @@ from ...core.threaded import execute_threaded
 from ...core.commandparser import CommandArguments, field_arg
 from ...core._validators import validate_float_non_negative, validate_number_unit, validate_number_positive
 from ...core.number import Number, parse_number, Unit
-from ...operations.fourier import flattop_window, fourier
+from ...operations.fourier import flattop_window, fourier, ifourier
 from ...core.context import Context
 from ...core.datastore import Domain
 
@@ -125,7 +125,7 @@ def _compute_background_fourier(xy: npt.NDArray[np.floating],
     W = flattop_window(r, backward_window_shape, 'hanning')
 
     f = fourier(x, y * x**kweight * w, r)
-    b = fourier(r, f.conj() * W, x)
+    b = ifourier(r, f * W, x)
 
     bkg = b.real / (x**kweight + sargs.epsilon) / w
 

src/estrapy/operations/fourier.py
@@ -141,11 +141,23 @@ def fourier(
     
     for j in prange(nr):
         real, imag = 0.0, 0.0
+        FTr = 2 * x[0] * r[j]                       # Initial right edge (i=0)
+        FTrc, FTrs = np.cos(FTr), np.sin(FTr)       # Initial cos/sin for right edge
         for i in range(len(x) - 1):
-            FTl = 2 * x[i] * r[j]
-            FTr = 2 * x[i + 1] * r[j]
-            real += yl[i] * np.cos(FTl) + yr[i] * np.cos(FTr)
-            imag += yl[i] * np.sin(FTl) + yr[i] * np.sin(FTr)
+            FTlc, FTls = FTrc, FTrs                 # Previous right edge becomes current left edge
+            FTr = 2 * x[i + 1] * r[j]               # New right edge
+            FTrc, FTrs = np.cos(FTr), np.sin(FTr)   # New cos/sin for right edge
+            real += yl[i] * FTlc + yr[i] * FTrc
+            imag -= yl[i] * FTls + yr[i] * FTrs
         result[j] = real + 1j * imag
     
     return result / np.sqrt(4 * np.pi)
+
+def ifourier(
+    r: npt.NDArray[np.floating],
+    F: npt.NDArray[np.complexfloating],
+    x: npt.NDArray[np.floating],
+) -> npt.NDArray[np.complexfloating]:
+    y = fourier(r, F.conj(), x).conj()
+
+    return y