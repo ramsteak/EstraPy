26c815f55d94710631738ee758b99ba90cf27dc5
Marco Stecca <marco.stecca99@gmail.com>
2026-05-14 12:17:18 +0200

    Refactor command parsing to store results in parsecontext and raise error for unimplemented exit command

--- src/estrapy/commands/__init__.py
+++ src/estrapy/commands/__init__.py
@@ -65,86 +65,87 @@ def parse_command(command: list[Token | Tree[Token]], parsecontext: ParseContext
         # Command filein ---------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'filein') as t, *args]:
             from .filein import Command_Filein
-            return Command_Filein.parse(t, args, parsecontext)
+            cmd = Command_Filein.parse(t, args, parsecontext)
         # Command align ----------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'align') as t, *args]:
             from .align import Command_Align
-            return Command_Align.parse(t, args, parsecontext)
+            cmd = Command_Align.parse(t, args, parsecontext)
         # Command edge -----------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'edge') as t, *args]:
             from .edge import Command_Edge
-            return Command_Edge.parse(t, args, parsecontext)
+            cmd = Command_Edge.parse(t, args, parsecontext)
         # Command noise ----------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'noise') as t, *args]:
             from .noise import Command_Noise
-            return Command_Noise.parse(t, args, parsecontext)
+            cmd = Command_Noise.parse(t, args, parsecontext)
         # Command preedge --------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'preedge') as t, *args]:
             from .preedge import Command_Preedge
-            return Command_Preedge.parse(t, args, parsecontext)
+            cmd = Command_Preedge.parse(t, args, parsecontext)
         # Command postedge -------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'postedge') as t, *args]:
             from .postedge import Command_Postedge
-            return Command_Postedge.parse(t, args, parsecontext)
+            cmd = Command_Postedge.parse(t, args, parsecontext)
         # Command normalize ------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'normalize') as t, *args]:
             from .normalize import Command_Normalize
-            return Command_Normalize.parse(t, args, parsecontext)
+            cmd = Command_Normalize.parse(t, args, parsecontext)
         # Command background ------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'background') as t, *args]:
             from .background import Command_Background
-            return Command_Background.parse(t, args, parsecontext)
+            cmd = Command_Background.parse(t, args, parsecontext)
         # Command fourier --------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'fourier') as t, *args]:
             from .fourier import Command_Fourier
-            return Command_Fourier.parse(t, args, parsecontext)
+            cmd = Command_Fourier.parse(t, args, parsecontext)
         # Command fit ------------------------------------------------------------------------------
         # case [Token('COMMANDNAME', 'fit') as t, *args]:
         #     from .fit import Command_Fit
-        #     return Command_Fit.parse(t, args, parsecontext)
+        #     cmd = Command_Fit.parse(t, args, parsecontext)
         # Command save -----------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'save') as t, *args]:
             from .save import Command_Save
-            return Command_Save.parse(t, args, parsecontext)
+            cmd = Command_Save.parse(t, args, parsecontext)
         # Command cut ------------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'cut') as t, *args]:
             from .cut import Command_Cut
-            return Command_Cut.parse(t, args, parsecontext)
+            cmd = Command_Cut.parse(t, args, parsecontext)
         # Commnand average ---------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'average') as t, *args]:
             from .average import Command_Average
-            return Command_Average.parse(t, args, parsecontext)
+            cmd = Command_Average.parse(t, args, parsecontext)
         # Command interpolate ----------------------------------------------------------------------
         case [Token('COMMANDNAME', 'interpolate') as t, *args]:
             from .interpolate import Command_Interpolate
-            return Command_Interpolate.parse(t, args, parsecontext)
+            cmd = Command_Interpolate.parse(t, args, parsecontext)
         # Command show -----------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'show') as t, *args]:
             from .show import Command_Show
-            return Command_Show.parse(t, args, parsecontext)
+            cmd = Command_Show.parse(t, args, parsecontext)
         # Command plot -----------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'plot') as t, *args]:
             from .plot import Command_Plot
-            return Command_Plot.parse(t, args, parsecontext)
+            cmd = Command_Plot.parse(t, args, parsecontext)
         # Command deglitch -------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'deglitch') as t, *args]:
             from .deglitch import Command_Deglitch
-            return Command_Deglitch.parse(t, args, parsecontext)
+            cmd = Command_Deglitch.parse(t, args, parsecontext)
         # Command multiedge ------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'multiedge') as t, *args]:
             from .multiedge import Command_MultiEdge
-            return Command_MultiEdge.parse(t, args, parsecontext)
+            cmd = Command_MultiEdge.parse(t, args, parsecontext)
         # Command rebin ----------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'rebin') as t, *args]:
             from .rebin import Command_Rebin
-            return Command_Rebin.parse(t, args, parsecontext)
+            cmd = Command_Rebin.parse(t, args, parsecontext)
         # Command filter ---------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'filter') as t, *args]:
             from .filter import Command_Filter
-            return Command_Filter.parse(t, args, parsecontext)
+            cmd = Command_Filter.parse(t, args, parsecontext)
         # Command exit -----------------------------------------------------------------------------
         case [Token('COMMANDNAME', 'exit' | 'quit') as t, *args]:
             pass # Exit is a statement that has no arguments. Everything after exit is ignored.
+            raise NotImplementedError('Exit command execution is not implemented.')
             # TODO: verify exit works
         # Unknown command --------------------------------------------------------------------------
         case [Token('COMMANDNAME', str(name)) as c, *args]:
@@ -154,5 +155,6 @@ def parse_command(command: list[Token | Tree[Token]], parsecontext: ParseContext
             raise CommandSyntaxError('Invalid command syntax', c)
         case _:
             raise CommandSyntaxError('Invalid command syntax')
-
-    raise NotImplementedError('Exit command execution is not implemented.')
+        
+    parsecontext.commands.append(cmd)
+    return cmd
